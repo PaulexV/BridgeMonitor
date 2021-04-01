4c3c2711bd5ece03442a1a034ef8b5b8ef4cd2b3
paverrier <paulalexis.verrier@ynov.com>
2021-04-01 19:40:29 +0200

    added date-time next closing and reopening - cause too

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using PontChaban.Models;
 
 namespace PontChaban.Controllers
@@ -20,7 +22,8 @@ namespace PontChaban.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var closing = GetBridgeInfosFromApi();
+            return View(closing);
         }
 
         public IActionResult Privacy()
@@ -32,6 +35,14 @@ namespace PontChaban.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private static readonly HttpClient client = new HttpClient();
+        private static List<BridgeInfos> GetBridgeInfosFromApi()
+        {
+            var stringTask = client.GetStringAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+            var myJsonResponse = stringTask.Result;
+            var result = JsonConvert.DeserializeObject<List<BridgeInfos>>(myJsonResponse);
+            return result;
         }
     }
 }
---
+++ Models/BridgeInfos.cs
@@ -0,0 +1,23 @@
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+
+using System;
+using Newtonsoft.Json;
+
+namespace PontChaban.Models
+{
+    public class BridgeInfos
+    {
+        [JsonProperty("boat_name")]
+        public string BoatName { get; set; }
+
+        [JsonProperty("closing_type")]
+        public string ClosingType { get; set; }
+
+        [JsonProperty("closing_date")]
+        public DateTime ClosingDate { get; set; }
+
+        [JsonProperty("reopening_date")]
+        public DateTime ReopeningDate { get; set; }
+    }
+
+}
